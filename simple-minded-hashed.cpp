6031019cbcdd8900044b69e89512b0027c3fa97b
Fernando Abraham Zegarra Maratuech <54364092+Fernandozs10@users.noreply.github.com>
2024-06-19 14:06:38 -0500

    Create simple-minded-hashed.cpp

---
+++ simple-minded-hashed.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+#include using namespace std;
+
+int DP[27][27][352]; /*[letter][length][sum]*/
+
+void build() {
+    int i, j, k;
+    memset(DP, 0, sizeof(DP));
+    DP[0][0][0] = 1;
+    for (i = 1; i <= 26; i++) {
+        for (j = 0; j <= i; j++) {
+            for (k = 0; k < 352; k++) {
+                DP[i][j][k] = DP[i - 1][j][k];
+                if (j > 0 && k >= i)
+                    DP[i][j][k] += DP[i - 1][j - 1][k - i];
+            }
+        }
+    }
+}
+
+int main() {
+    build();
+    int L, S, Case = 0;
+    while (std::cin >> L >> S) {
+        if (L == 0 && S == 0)
+            break;
+        std::cout << "Case " << ++Case << ": ";
+        if (L > 26 || S > 351)
+            std::cout << "0\n";
+        else
+            std::cout << DP[26][L][S] << "\n";
+    }
+    return 0;
+}
